14b1629285f27c0af75bca5996a5dfadd07e63c2
kevin <kevin1737@163.com>
2021-12-16 20:04:39 +0800

    Update: Data Handle

--- PythonModules/Pandas/Examples/FliterData/filterData.py
+++ PythonModules/Pandas/Examples/FliterData/filterData.py
@@ -1,27 +1,37 @@
 from datetime import date
+from numpy import NaN
 import pandas as pd
 import os
 import openpyxl
-inputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/SourceData/'
-outputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/FilterData/'
-averageOutputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/AverageData/'
 
-def cleanFZTHerbList(FZTHerbPath, FZT_TTEHerbPath):
-    FZTdataFrame = pd.read_excel(FZTHerbPath)
-    FZTHerbList = FZTdataFrame['Herb name in Chinese'].values
-    print('FZTdataFrame:\n', FZTdataFrame)
-    FZTdataFrame.set_index('Herb name in Chinese', inplace=True)
-    FZT_TTEdataFrame = pd.read_excel(FZT_TTEHerbPath)
+outputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/FliterData/328HerbsInfo/'
+
+def cleanFZTHerbList(Herbs402Path, Herbs703Path):
+    Herbs402dataFrame = pd.read_excel(Herbs402Path)
+    Herbs402List = Herbs402dataFrame['Herb name in Chinese'].values
+    #print('Herbs402List:\n', Herbs402List)
+    #FZTdataFrame.set_index('Herb name in Chinese', inplace=True)
+    Herbs703dataFrame = pd.read_excel(Herbs703Path, sheet_name='SimpleDataSet')
     #print('FZT_TTEdataFrame:\n', FZT_TTEdataFrame)
-    FZT_TTEHerbList = FZT_TTEdataFrame['MoleculeName'].values
+    Herbs502List = Herbs703dataFrame['Herb name in Chinese'].values
     #FZT_TTEdataFrame.set_index('MoleculeName', inplace=True)
     
-    print('FZT_TTEdataFrame:\n', FZT_TTEdataFrame)
-    print('FZT_TTEHerbList:\n', FZT_TTEHerbList)
-
-    for FZTHerb in FZTHerbList:
-        if FZTHerb in FZT_TTEHerbList:
-            continue
-        FZTdataFrame.drop(index=FZTHerb, inplace=True)
-    FZTdataFrame.to_excel(averageOutputPath+'Experiment.xlsx')
+    #print('Herbs502List:\n', Herbs502List)
+    #print('Herbs502dataFrame:\n', Herbs502dataFrame)
+    result = pd.DataFrame()
+    for HerbName in Herbs402List:
+        print('HerbName:\n',HerbName)
+        if HerbName in Herbs502List:
+            selectHerbInfo = Herbs703dataFrame.loc[Herbs703dataFrame['Herb name in Chinese'].isin([HerbName])]
+        else:
+            selectHerbInfo = pd.DataFrame({'Herb name in Chinese':[HerbName],'Function':[NaN]})
+        print('selectHerbInfo:',selectHerbInfo)
+        result = result.append(selectHerbInfo, ignore_index=True)
+    print('result:\n', result)
+    result.to_excel(outputPath+'402_703HerbInfo.xlsx')
     #return FZTHerbList
+
+if __name__ == '__main__':
+    Herbs402Path = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/FliterData/502HerbsData/402中药信息.xlsx'
+    Herbs703Path = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/FliterData/703HerbsData/703Herbs.xlsx'
+    cleanFZTHerbList(Herbs402Path, Herbs703Path)
