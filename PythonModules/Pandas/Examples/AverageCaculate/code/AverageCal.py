2553d1a123209ba5201f14e1dd583f38f0a6d7a0
kevin <kevin1737@163.com>
2022-03-06 16:36:12 +0800

    Update: Scolar Program Notes

--- PythonModules/Pandas/Examples/AverageCaculate/code/AverageCal.py
+++ PythonModules/Pandas/Examples/AverageCaculate/code/AverageCal.py
@@ -3,15 +3,15 @@ import pandas as pd
 import os
 import openpyxl
 inputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/SourceData/'
-outputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/FliterData/502HerbsData/502HerbsPharmacology/'
+outputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/AverageCaculate/AverageData/ExperimentFilterData/'
 averageOutputPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/FliterData/502HerbsData/502HerbsAverageData/'
-HerbPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/AverageCaculate/AverageData/328中药信息.xlsx'
-sheetName = 'Table'
+HerbPath = '/Users/kevin/Desktop/program files/python/PythonModules/Pandas/Examples/AverageCaculate/AverageData/ExperimentHerbsData.xlsx'
+sheetName = 'Sheet1'
 
 def getHerbList(HerbPath):
     dataFrame = pd.read_excel(HerbPath, sheet_name=sheetName)
-    FZTHerbList = dataFrame['Herb Name'].values
-    print('herbNameList:\n', FZTHerbList)
+    FZTHerbList = dataFrame['herb'].values
+    #print('herbNameList:\n', FZTHerbList)
     return FZTHerbList
 
 def screenExcelFiles(inputPath, FZTHerbList):
@@ -19,19 +19,26 @@ def screenExcelFiles(inputPath, FZTHerbList):
     fileHerbNames = [fileName.split('.')[0] for fileName in filesNames]
     i=1
     herbAverageDataFrame = pd.DataFrame()
+    screenHerbsList = []
     for herbName in FZTHerbList:
         #herbName = fileName.split('.')[0]
         #postFix = fileName.split('.')[1]
         if herbName not in fileHerbNames:
             continue
+        screenHerbsList.append(herbName)
         '''
         if herbName not in fileHerbNames:
             continue
         '''
         print(f'handle herbName:{herbName}')
         #fileName = herbName+'.xlsx'
-        AverageDataFrame = filterData(herbName, i, herbAverageDataFrame)
+        #AverageDataFrame = filterData(herbName, i, herbAverageDataFrame)
+        filterData(herbName, i , herbAverageDataFrame)
         #print('AverageDataFrame:\n', AverageDataFrame)
+        i+=1
+    dataFrame = pd.DataFrame(data=screenHerbsList, columns=['herb'])
+    dataFrame.to_excel(outputPath+'screenHerbsList.xlsx')
+    '''
         herbAverageDataFrame = herbAverageDataFrame.append(AverageDataFrame, ignore_index=True)
         i+=1
     herbAverageDataFrame.set_index('MoleculeName', inplace=True)
@@ -40,6 +47,7 @@ def screenExcelFiles(inputPath, FZTHerbList):
     print('herbAverageDataFrame:\n',herbAverageDataFrame)
     herbAverageDataFrame.to_excel(averageOutputPath+'502HerbsPharmacologyAverage1225.xlsx')
     print(f'Write to Excel file Successfully!')
+    '''
 
 def filterData(herbName, index, herbAverageDataFrame):
     dataFrame = pd.read_excel(inputPath+herbName+'.xlsx')
@@ -51,7 +59,8 @@ def filterData(herbName, index, herbAverageDataFrame):
     dataFrame.set_index('MolID', inplace=True)
     dataFrame.dropna(subset=['HL'], inplace = True)
     #print('dataFrame:\n', dataFrame)
-    dataFrame = dataFrame[(dataFrame['DL']>=0.5) & (dataFrame['OB(%)']>=35)].sort_values(by='OB(%)',ascending=False)
+    dataFrame = dataFrame[(dataFrame['DL']>=0.18) & (dataFrame['OB(%)']>=40)].sort_values(by='OB(%)',ascending=False)
+    '''
     columnMean = dataFrame[['MW','AlogP','Hdon','Hacc','OB(%)','Caco-2','BBB','DL','FASA-','HL']].mean()
     #columnMean['MolID'] = 'Average'
     #print('columnMean:\n', columnMean)
@@ -60,9 +69,10 @@ def filterData(herbName, index, herbAverageDataFrame):
     dataFrame = dataFrame.append(columnMean, ignore_index=True)
     AverageDataFrame = columnMean
     #print('herbAverageDataFrame:\n', herbAverageDataFrame)
-    dataFrame.to_excel(outputPath+str(index)+herbName+'.xlsx')
+    '''
+    dataFrame.to_excel(outputPath+herbName+'.xlsx')
     print(f'Write {index}: {herbName} to Excel file Successfully!')
-    return AverageDataFrame
+    #return dataFrame
     
 if __name__ == '__main__':
     HerbList = getHerbList(HerbPath)
